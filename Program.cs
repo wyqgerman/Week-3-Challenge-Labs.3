9bebe173c00d7daa6138a3d02eec4880c2d98d39
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-21 16:57:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Week_3_Challenge_Labs._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = TwoSum(nums, target);
+
+            if (result != null)
+            {
+                Console.WriteLine($"[{result[0]}, {result[1]}]");
+            }
+            else
+            {
+                Console.WriteLine("No solution found.");
+            }
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numIndexMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (numIndexMap.ContainsKey(complement))
+                {
+                    return new int[] { numIndexMap[complement], i };
+                }
+
+                if (!numIndexMap.ContainsKey(nums[i]))
+                {
+                    numIndexMap[nums[i]] = i;
+                }
+            }
+
+            return null;
+        }
+    }
+}
